4351eab0751e60272a747eb8a46064aca3190651
Tixoni <154960079+Tixoni@users.noreply.github.com>
2025-11-05 15:47:36 +0300

    tteaaaaasrttttt

--- пример/backend/data_service/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional, Dict, Any
-
-class FileMetadataCreate(BaseModel):
-    filename: str
-    filetype: str
-    title: Optional[str] = None
-    description: Optional[str] = None
